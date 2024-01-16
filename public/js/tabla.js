25b44e38ee50c80be3810dbad6447b9135caabf9
lucasmouhsen <lucasmouhsen@gmail.com>
2024-01-16 15:01:07 -0300

    Cambio de url

--- public/js/tabla.js
+++ public/js/tabla.js
@@ -26,7 +26,7 @@ const dataTableOptions = {
     serverSide: true,
     deferRender: true,
     ajax: {
-        url: 'http://localhost:3001/historias',
+        url: 'http://172.19.40.21/:3001/historias',
         type: 'get',
     },
     columns: [
